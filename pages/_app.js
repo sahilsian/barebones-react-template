46410c08b3f454b2acd579420ccd409f7c31c977
Sahil Sian <sh864246@dal.ca>
2025-02-09 22:45:25 -0400

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import Provider from '../src/utils/provider';
+import { Analytics } from "@vercel/analytics/react"
 
 function App({ Component, pageProps }) {
   return (
     <Provider>
     <div className="App">
       <Component {...pageProps} />
+      <Analytics></Analytics>
     </div>
     </Provider>
   );
