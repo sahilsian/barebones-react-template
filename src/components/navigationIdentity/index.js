812c8565eb639c7684365f772ac0db33a3cfa0e2
Sahil Sian <sh864246@dal.ca>
2025-02-09 23:10:38 -0400

    .

--- src/components/navigationIdentity/index.js
+++ src/components/navigationIdentity/index.js
@@ -1,17 +1,20 @@
+import Link from "next/link"
 import Logo from "../logo"
 import Text from "../text"
 
 const NavigationIdentity = ({heading="Rec N Roll", subheading="Campbellton, NB"}) => {
     return (
-        <div className="flex gap-5">
-            <div className="border-1px">
-                <Logo></Logo>
+        <Link href={"/"}>
+            <div className="flex gap-5">
+                <div className="border-1px">
+                    <Logo></Logo>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <Text color={"#FFFFFF"} level={"4"} text={heading}></Text>
+                    <Text color={"#FFFFFF"} level={"6"} text={subheading}></Text>
+                </div>
             </div>
-            <div className="flex flex-col justify-center">
-                <Text color={"#FFFFFF"} level={"4"} text={heading}></Text>
-                <Text color={"#FFFFFF"} level={"6"} text={subheading}></Text>
-            </div>
-        </div>
+        </Link>
     )
 }
 
